f6d2450ceaa722f14c5211939a890bfccffab7c8
aatif_tgbtt <aatif@healthdekho.com>
2020-07-25 20:21:18 +0530

    removed unwanted feature code

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
+
 import feedsReducer from "../features/feeds/feedSlice";
 
 export default configureStore({
     reducer: {
         feed: feedsReducer,
-        counter: counterReducer,
     },
 });
